cade39fdb49c8062e1bcf0221389201e0451f255
MrMalibu333 <Speedy317@yahoo.com>
2025-03-02 11:00:30 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the PsyC Genius Test!</h1>
+      <p>Your interactive transformation quiz is coming soon.</p>
+    </div>
+  );
+}
 
